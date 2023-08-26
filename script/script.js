d2ec6e4630b9992b94c65d45bdbd3c50119044e2
Samuel <22201394@aluno.cotemig.com.br>
2023-08-25 21:06:15 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.amazing', { scale:0.75, duration: 2000 });
+sr.reveal('.botao', {rotate:{x:10,y:0,z:0}, duration: 2100})
+sr.reveal('.history', {rotate: {x: 100, y: 0, z: 0}, duration:3000});
+sr.reveal('.menu', {rotate: {x: 100, y: 0, z: 0}, duration:3000} );
+sr.reveal('.cards', {easing: 'ease-out',duration: 3000});
+sr.reveal('.img-paodequeijo', { scale: 0.9,duration: 3000 });   
+sr.reveal('.img-panqueca', { scale: 0.9,duration: 3000 });
+
+
